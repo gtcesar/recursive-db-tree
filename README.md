d1041a07291cfe6a959529043d4257a7168da8b2
gtcesar <gtcesar@hotmail.com>
2024-05-27 18:53:05 -0300

    Initial commit for project, version v0.0.1

--- README.md
+++ README.md
@@ -1,2 +1,78 @@
-# recursive-db-tree
-A versatile and efficient recursive database tree component for Adianti Framework, empowering developers to easily implement hierarchical data structures with intuitive tree navigation and manipulation capabilities.
+# RecursiveDBTree Component <img src="https://img.shields.io/badge/Versão-0.0.1-green"> <img src="https://img.shields.io/badge/Licença-MIT-success"> <img src="https://img.shields.io/badge/Adianti-7.x-blue"> <img src="https://img.shields.io/badge/PHP-7.x-blueviolet">
+
+The RecursiveDBTree component is a versatile and efficient solution tailored for the Adianti Framework. It provides developers with a powerful toolset to manage hierarchical data structures seamlessly. This component facilitates intuitive tree navigation and manipulation within your web application.
+
+## Key Features
+- **Versatile:** Handles a wide range of hierarchical data structures.
+- **Efficient:** Optimized for performance and resource utilization.
+- **Intuitive Navigation:** Provides an intuitive interface for navigating through the tree structure.
+- **Easy Manipulation:** Allows for easy manipulation of tree elements, including adding, editing, and deleting nodes.
+
+## Based on VanillaTree Library
+This component is built upon the foundation of the [VanillaTree](https://github.com/finom/vanillatree) JavaScript library. Leveraging the capabilities of VanillaTree, the RecursiveDBTree component enhances the Adianti Framework by empowering developers to implement dynamic and interactive tree views effortlessly.
+
+## Installation
+Run the following command:
+`composer require gtcesar/recursive-db-tree`
+
+## Usage Example - RecursiveDBTree
+```sql
+CREATE TABLE IF NOT EXISTS `segmento` (
+  `id` int(11) NOT NULL,
+  `segmento_id` int(11) DEFAULT NULL,
+  `descricao` varchar(200) COLLATE utf8_unicode_ci NOT NULL
+) ENGINE=InnoDB AUTO_INCREMENT=18 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+INSERT INTO `segmento` (`id`, `segmento_id`, `descricao`) VALUES
+(1, NULL, 'Transporte'),
+(2, 1, 'Executivo'),
+(3, 1, 'Fracionado'),
+(4, NULL, 'Informática'),
+(5, 4, 'Software'),
+(6, 4, 'Suporte e manutenção'),
+(7, NULL, 'Varejo'),
+(8, 7, 'Materiais de limpeza'),
+(16, 8, 'Químicos'),
+(17, 16, 'Controlados');
+
+```
+```php
+
+use Gtcesar\RecursiveDBTree\RecursiveDBTree;
+
+class SegmentoForm extends TPage
+{
+    /**
+     * Class constructor
+     * Creates the page
+     */
+    function __construct()
+    {
+        parent::__construct();
+        
+        // creates a panel
+        $panel = new TPanelGroup('Segmentos');
+       
+        $segmentos = new RecursiveDBTree('segmento', 'DATABASE', 'Segmento', 'id', 'segmento_id', 'descricao', 'id asc');
+        $segmentos->collapse();
+        $segmentos->setItemAction(new TAction(array($this, 'onSelect')));
+        
+        $panel->add($segmentos);
+        
+        // wrap the page content using vertical box
+        $vbox = new TVBox;
+        $vbox->style = 'width: 100%';
+        $vbox->add($panel);
+
+        parent::add($vbox);
+    }
+    
+    public function onSelect($param)
+    {
+        new TMessage('info', str_replace(',', '<br>', json_encode($param)));
+    }    
+
+}
+```
+## Resultado
+<img src="https://github.com/gtcesar/recursive-db-tree/blob/main/images/RecursiveDBTree.png?raw=true">
---
+++ src/RecursiveDBTree.php
@@ -0,0 +1,162 @@
+<?php
+
+/**
+ * RecursiveDBTree
+ *
+ * @version    v0.0.1
+ * @author     Augusto César da Costa Marques
+ * @copyright  Copyright (c) 2021 Augusto César da Costa Marques
+ * @license    MIT License
+ */
+namespace Gtcesar\RecursiveDBTree;
+
+use Adianti\Widget\Base\TElement;
+use Adianti\Widget\Base\TStyle;
+use Adianti\Database\TCriteria;
+use Adianti\Widget\Wrapper\AdiantiDatabaseWidgetTrait;
+
+/**
+ * This class represents a recursive database tree component.
+ * It is designed to handle hierarchical data structures efficiently.
+ * Developers can easily implement hierarchical data structures with intuitive tree navigation and manipulation capabilities.
+ * This component is based on the VanillaTree JavaScript library.
+ */
+class RecursiveDBTree extends TElement
+{
+    protected $items;
+    private $tagname;
+    private $itemAction;
+    private $collapsed;
+    private $key;
+    private $label;
+    private $parentkey;
+
+    /**
+     * Class Constructor
+     * @param string $tagname       The HTML tag name of the main element
+     * @param string $database      The database name
+     * @param string $model         The model class name
+     * @param string $key           The table field to be used as a key in the tree
+     * @param string $parentkey     The table field used as a parent reference
+     * @param string $label         The table value to be used as label
+     * @param string $ordercolumn   The column to order the fields (optional)
+     * @param TCriteria $criteria   The criteria (TCriteria object) to filter the model (optional)
+     */
+    public function __construct($tagname, $database, $model, $key, $parentkey, $label, $ordercolumn = null, TCriteria $criteria = null)
+    {
+        parent::__construct('main');
+        $this->id = 'tree_' . uniqid();
+        $this->tagname = $tagname;
+        $this->key = $key;
+        $this->parentkey = $parentkey;
+        $this->label = $label;
+        $this->collapsed = false;
+        $this->items = AdiantiDatabaseWidgetTrait::getObjectsFromModel($database, $model, $key, $ordercolumn, $criteria);
+    }
+
+    /**
+     * Collapse the Tree
+     * This method sets the tree to be collapsed, i.e., all nodes are closed.
+     */
+    public function collapse()
+    {
+        $this->collapsed = true;
+    }
+
+    /**
+     * Set item action
+     * @param mixed $action   The item action
+     * This method sets an action to be executed when an item in the tree is selected.
+     * The action can be a URL or an instance of TAction.
+     */
+    public function setItemAction($action)
+    {
+        $this->itemAction = $action;
+    }
+
+    /**
+     * Shows the plugin at the screen
+     * This method renders the RecursiveDBTree component on the screen.
+     * It generates the necessary HTML and JavaScript to display the tree.
+     */
+    public function show()
+    {
+    	// Include the JavaScript library
+    	$include = new TElement('script');    	
+    	$include->src = 'vendor/gtcesar/recursive-db-tree/src/js/vanillatree.min.js';
+    	$include->type = 'text/javascript';
+    	parent::add($include);
+
+        // Check if the tree is collapsed
+        $collapsed = $this->collapsed ? 'true' : 'false';
+        $constante = $this->id;
+
+        // Create the script element
+        $script = new TElement('script');
+        $script->type = 'text/javascript';
+
+        // Create the main element to hold the tree
+        $output = new TElement($this->tagname);
+        parent::add($output);
+
+        // Add the JavaScript code to initialize the tree
+        $script->add(
+            '
+            $(document).ready(function(){ const ' .
+            $this->tagname .
+            '  = document.querySelector(' .
+            "'{$this->tagname}'" .
+            ');
+                const ' . $constante . ' = new VanillaTree(' .
+            "{$this->tagname}" .
+            ');
+        '
+        );
+
+        // Iterate over the items and add them to the tree
+        foreach ($this->items as $item) {
+
+            $key = $item->{$this->key};
+            $label = $item->{$this->label};
+            $parentkey = $item->{$this->parentkey};
+
+            // Build the tree node
+            $tree = "{$constante}.add({label: '{$label}',";
+
+            if ($parentkey != null) {
+                $tree .= "parent: '{$parentkey}',";
+            }
+            $tree .= "id: '{$key}',";
+            $tree .= "opened: {$collapsed}});";
+
+            // Add the tree node to the script
+            $script->add($tree);
+        }
+
+        // Add action event listener to the tree
+        if ($this->itemAction) {
+            $string_action = $this->itemAction->serialize(FALSE);
+            $script->add('
+            ' . $this->tagname . '.addEventListener("vtree-select", function(evt) {
+                __adianti_load_page("index.php?' . $string_action . '&key=" + evt.detail.id);
+            });
+            ');
+        }
+
+        // Close the script tag
+        $script->add('
+        });
+        ');
+
+        // Add the script to the main element
+        parent::add($script);
+        
+        // Import the CSS file for the tree
+        TStyle::importFromFile('vendor/gtcesar/recursive-db-tree/src/css/vanillatree.min.css');
+
+        // Render the main element
+        parent::show();
+    }
+}
+
+?>
